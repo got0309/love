231b5854f93eddb701206cf4db0085e323b2b978
Sung Hun Go <sungtheusual@gmail.com>
2021-09-19 01:06:51 +0900

    몰라이젠

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function check(form) {
-    if(form.password.value=="20180916") {
-        location.href='home.html'
+function check() {
+    if(password.value==20180916) {
+        location.href='home.html';
     } else {
-        alert("땡 틀렸답니다!")
+        alert("땡 틀렸답니다!");
     }
 }
 
-function check2(form) {
-    if(form.password=="20180916") {
-        location.href='home.html'
+function check2() {
+    if(password.value==20180916) {
+        location.href='home.html';
     } else {
-        alert("땡 틀렸답니다!")
+        alert("땡 틀렸답니다!");
     }
 }
