86d6a71018dea0a382a5e11ff0c7a496ee341bcb
Sung Hun Go <sungtheusual@gmail.com>
2021-09-19 02:29:47 +0900

    엔터키 해결
    엔터키와 클릭 submit이 같이 동작합니다!

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function check() {
-    if(password.value==20180916) {
-        location.href='home.html';
-    } else {
-        alert("땡 틀렸답니다!");
-    }
-}
+const pwd = document.getElementById("password")
 
-function check2() {
-    if(password.value==20180916) {
+function check(event) {
+    event.preventDefault();
+    console.log(pwd.value);
+    if(pwd.value=="20180916") {
         location.href='home.html';
     } else {
         alert("땡 틀렸답니다!");
     }
-}
+    return false;
+} 
