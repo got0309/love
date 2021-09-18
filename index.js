19f22b94974f2d09a9fd79712bd5f6087f120323
Sung Hun Go <sungtheusual@gmail.com>
2021-09-18 23:39:09 +0900

    엔터키=제출 만들기

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function check(form) {
         alert("땡 틀렸답니다!")
     }
 }
+
+function check2(form) {
+    if(form.password=="20180916") {
+        location.href='home.html'
+    } else {
+        alert("땡 틀렸답니다!")
+    }
+}
